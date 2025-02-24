ad96ae4fbfa94f698e9ad1f80472c8f0ffbba938
Tim Sykes <timsykes@tims-air.lan>
2025-02-24 22:03:43 +1100

    clean up layout and page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 export default function Home() {
   return (
-    <div className="min-h-screen p-8">
-      <main className="max-w-4xl mx-auto">
-        <h1 className="text-4xl font-bold mb-8">Downsizing Calculator</h1>
-        {/* We'll add your calculator components here */}
-      </main>
+    <div className="min-h-screen p-4">
+      <h1 className="text-4xl font-bold">Downsizing Calculator</h1>
     </div>
   );
 }
