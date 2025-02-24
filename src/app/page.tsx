03154038e4a28a969b53899bb9af6606e99e0ddd
Tim Sykes <timsykes@tims-air.lan>
2025-02-24 22:26:51 +1100

    remove old React files, clean up structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
 export default function Home() {
   return (
-    <div className="min-h-screen p-8 bg-white dark:bg-black">
-      <main className="max-w-4xl mx-auto">
-        <h1 className="text-4xl font-bold text-black dark:text-white">
-          Downsizing Calculator
-        </h1>
-        <p className="mt-4 text-gray-600 dark:text-gray-300">
-          Welcome to your downsizing calculator
-        </p>
-      </main>
+    <div className="p-8">
+      <h1 className="text-4xl font-bold">Testing</h1>
+      <p className="mt-4">If you see this, it worked!</p>
     </div>
-  );
+  )
 }
